d884e25ec22a5e018fa9f5977ad77bb2fc0b4bab
ikovic <kovic.ivan@gmail.com>
2023-08-24 15:08:52 +0200

    Remove unused files and fix the dark mode issue

--- apps/demo/src/pages/index.tsx
+++ apps/demo/src/pages/index.tsx
@@ -36,10 +36,9 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title="/id | Docusaurus"
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
