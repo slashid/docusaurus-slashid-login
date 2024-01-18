0770e7c1550b3c94451a981481591a28ee1f5e84
ikovic <kovic.ivan@gmail.com>
2024-01-18 14:53:38 +0100

    Protect the DocCategoryGeneratedIndexPage

--- packages/docusaurus-theme-slashid/src/theme/DocCategoryGeneratedIndexPage/index.tsx
+++ packages/docusaurus-theme-slashid/src/theme/DocCategoryGeneratedIndexPage/index.tsx
@@ -8,12 +8,25 @@
 import React from "react";
 
 import type { PropCategoryGeneratedIndex } from "@docusaurus/plugin-content-docs";
+import { Redirect } from "@docusaurus/router";
+import { useSlashID } from "@slashid/react";
 import DocCategoryGeneratedIndexPage from "@theme-init/DocCategoryGeneratedIndexPage";
+
+import { shouldPathRender } from "../../domain";
+import { useSlashIDConfig } from "../hooks/useSlashIDConfig";
 
 export type Props = {
   categoryGeneratedIndex: PropCategoryGeneratedIndex;
 };
 
 export default function DocCategoryGeneratedIndexPageWrapper(props: any) {
+  const { user } = useSlashID();
+  const config = useSlashIDConfig();
+  const redirectTo = config.privateRedirectPath!;
+
+  if (!shouldPathRender(props.route.path, config.privatePaths, user)) {
+    return <Redirect to={redirectTo} />;
+  }
+
   return <DocCategoryGeneratedIndexPage {...props} />;
 }
